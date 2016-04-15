9409e0e2be69b5fb2f353f0ddd73df9edc88aa8d
kwnminho <kwnminho@gmail.com>
2016-04-15 16:57:34 -0500

    modified

--- Rigol_Scope.py
+++ Rigol_Scope.py
@@ -3,3 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plot
 import time
 import datetime
+rm = visa.ResourceManager()
+rm.list_resources()
+scope = rm.open_resource('TCPIP0::169.254.1.5::INSTR',read_termination='\n')
+print(scope.query('*IDN?'))
